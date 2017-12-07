38e50ec05c51ca38fc8581100234fd20dad80b67
Sơn Bùi <buison559@gmail.com>
2017-12-07 18:15:38 +0700

    Xong hàm tính điểm TB và điểm RL cho 1 SV, cần hàm tính điểm RL cho nhiều SV

---
+++ Project_Team/View/TestForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Team
+{
+    public partial class TestForm : Form
+    {
+        Manager_DAL dal;
+        public TestForm()
+        {
+            dal = new Manager_DAL();
+            InitializeComponent();
+            dal.TinhDTB_TatCacMon_DAL();
+            dataGridView1.DataSource = dal.ShowKetQua(); 
+        }
+    }
+}
